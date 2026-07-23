802ab754c649a5819493248a9d6f13f42e7f6e25
Arun <arunjarpula5@gmail.com>
2026-07-24 00:08:45 +0530

    Enable debug

--- EShoping/settings.py
+++ EShoping/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e0_9*15+hr1uhamrz6^vj7ykskfdg5kzs3iym)1ri&q+e3&h0e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
