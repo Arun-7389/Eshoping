20b03e1a5bfde644d7544db49ed6d8ec294caf16
Arun <arunjarpula5@gmail.com>
2026-09-19 18:00:10 +0530

    Fix media file serving on Render

--- EShoping/urls.py
+++ EShoping/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf.urls.static import static
 
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 from drf_spectacular.views import SpectacularAPIView,SpectacularSwaggerView,SpectacularRedocView
+
 
 urlpatterns = [
     path('Mystore_admin/', admin.site.urls),
@@ -36,4 +37,15 @@ urlpatterns = [
 
     path('api/token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
     path('api/token/refresh/',TokenRefreshView.as_view(),name='token_refresh'),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+    ]
+from django.views.static import serve
+from django.urls import re_path
+
+
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT
+    }),
+]
